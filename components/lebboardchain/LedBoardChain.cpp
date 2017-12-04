678e2efa6fe23e3cdbadac264a03ca255742e9e5
Wouter Devinck <wouter.devinck@barco.com>
2017-12-04 18:04:49 +0100

    Blank implementation of LedBoardChain interface

---
+++ components/lebboardchain/LedBoardChain.cpp
@@ -0,0 +1,11 @@
+#include "LedBoardChain.h"
+
+using namespace lamp;
+
+LedBoardChain::LedBoardChain() {
+  // TODO
+}
+
+void LedBoardChain::addKeyframe(milliseconds duration, vector<LedValue> keyframe) {
+  // TODO
+}
